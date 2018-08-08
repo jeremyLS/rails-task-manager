ff6e8776cccd3464e6b232be6b2cf3ca696fefd2
Jeremy Le Strat <jeremy.lstrat@gmail.com>
2018-08-08 11:19:20 +0200

    rails exo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,49 @@
 class TasksController < ApplicationController
-  def title
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def list
+    @tasks = Task.all
+  end
+  def show
+    @task = Task.find(params[:id])
+  end
+  def new
+    task = Task.new
+  end
+  def create
+      # TODO:
+    # 1. Instancier une task object à partir de params[:task]
+    task = Task.new(task_params)
+
+    # 2. Save
+    task.save
+    redirect_to tasks_path(task)
   end
 
-  def details
+  def edit
+  end
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+  def destroy
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
   end
 
-  def completed
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
   end
+
+
+  def task_params
+    # strong_params: whitelist params coming from potentially malicious user
+    params.require(:task).permit(:title, :details)
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  get 'tasks/title'
-  get 'tasks/details'
-  get 'tasks/completed'
+  get "tasks/new", to: "tasks#new"
+  get "tasks", to: "tasks#list"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
